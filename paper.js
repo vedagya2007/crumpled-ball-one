e4d9d91c63bfc51b7c73603c75beb95e84f85519
vedagya2007 <74814960+vedagya2007@users.noreply.github.com>
2020-12-03 11:51:47 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,9 +1,9 @@
 class Paper {  
     constructor(x, y, radius){
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2,
             'isStatic' : false
         }
          this.radius = radius
